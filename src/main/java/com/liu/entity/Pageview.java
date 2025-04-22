75b614ae2020ba1030851efaaef79901266a80b2
strive <2205487298@qq.com>
2025-04-22 17:25:21 +0800

    update entity

--- src/main/java/com/liu/entity/Pageview.java
+++ src/main/java/com/liu/entity/Pageview.java
@@ -1,6 +1,39 @@
 package com.liu.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * 浏览实体
+ *
+ * @author liu
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Accessors(chain = true)
+@TableName("pageview")
 public class Pageview {
 
+    /**
+     * 浏览量主键id
+     */
+    private int id;
+
+    /**
+     * 浏览量
+     */
+    private Integer pageviews;
+
+
+    /**
+     * 病的id
+     */
+    private Integer illnessId;
 
 }
