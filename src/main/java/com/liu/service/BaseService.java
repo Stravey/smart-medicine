30c63c4af26a0ba92758354f4f71f597cff55c9a
strive <2205487298@qq.com>
2025-04-22 17:50:30 +0800

    update dto service utils

--- src/main/java/com/liu/service/BaseService.java
+++ src/main/java/com/liu/service/BaseService.java
@@ -1,7 +1,37 @@
 package com.liu.service;
 
-import java.io.Serializable;
+import com.liu.dao.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * 基础服务类
+ *
+ * @author rui
+ */
 public abstract class BaseService<T> implements IService<T> {
+
+    @Autowired
+    protected UserDao userDao;
+
+    @Autowired
+    protected HistoryDao historyDao;
+
+    @Autowired
+    protected IllnessDao illnessDao;
+
+    @Autowired
+    protected IllnessKindDao illnessKindDao;
+
+    @Autowired
+    protected IllnessMedicineDao illnessMedicineDao;
+
+    @Autowired
+    protected MedicineDao medicineDao;
+
+    @Autowired
+    protected MedicalNewsDao medicalNewsDao;
+
+    @Autowired
+    protected PageviewDao pageviewDao;
 
 }
